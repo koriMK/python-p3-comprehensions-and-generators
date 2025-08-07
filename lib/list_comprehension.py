afc3487b58ac839949c460b179e926e6b5695d05
Joseph Mwangi <korimwangi01@gmail.com>
2025-08-07 14:34:41 -0400

    initial commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
-
+num = 0, 1, 3, 5, 7, 8, 9
 def return_evens(num_list):
+    return [num for num in num_list if num % 2 == 0]
     pass
 
+    make_exclamation(["Hello", "I'm doing great", "Python is fun"])
+
 def make_exclamation(sentence_list):
+
+    return [sentence + '!' for sentence in sentence_list]
     pass
